e1897dc397aed723493616c7a0ac9c5f78a9cc31
Daryl Thomas <thomastw935@gmail.com>
2021-10-02 15:45:48 +0800

    Added a theme

---
+++ src/core-ui/theme.ts
@@ -0,0 +1,19 @@
+import { DefaultTheme } from "styled-components"
+
+const defaultTheme: DefaultTheme = {
+  device: {
+    sm: "425px",
+    md: "768px",
+    lg: "1024px",
+  },
+  palette: {
+    primary: "#fff",
+    secondary: {
+      lighter: "#343434",
+      light: "#1f1f1f",
+      normal: "#131313",
+      dark: "#060606",
+    },
+  },
+}
+export default defaultTheme
