d547d1c185edc35c62d4fb78d08dfcb45ca6f239
Daryl Thomas <thomastw935@gmail.com>
2021-10-05 17:32:31 +0800

    Added Converter component and style

--- src/components/Converter/Converter.style.ts
+++ src/components/Converter/Converter.style.ts
@@ -1,1 +1,36 @@
-import styled from "styled-components"
+import styled, { StyledComponentBase } from "styled-components"
+
+interface ICon extends StyledComponentBase<any, {}> {
+  Display?: any
+  Buttons?: any
+}
+
+const Con: ICon = styled.div`
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  padding-top: 0;
+`
+
+const display = styled.section`
+  div {
+    padding-bottom: 1rem;
+  }
+  select {
+    display: block;
+    /* font-size: 1.5rem; */
+    text-transform: capitalize;
+  }
+`
+
+const buttons = styled.section`
+  display: grid;
+  grid-template: repeat(5, 1fr) / repeat(3, 1fr);
+  gap: 0.3em;
+  height: 100%;
+`
+
+Con.Display = display
+Con.Buttons = buttons
+
+export default Con
--- src/components/Converter/index.tsx
+++ src/components/Converter/index.tsx
@@ -1,3 +1,51 @@
+import { useState, useEffect } from "react"
+import Button from "../../core-ui/Button"
+import { converterButtons, SPECIAL_BUTTONS } from "../../Data/Buttons"
+
+import Con from "./Converter.style"
+import Input from "../../core-ui/Input"
+import { unit } from "mathjs"
+import useConverter from "./hooks/useConverter"
+import Tile from "./Tile"
+
 export default function Converter() {
-  return <div></div>
+  const base = "length"
+  const { tiles, dispatch, handleUnit, activeTile, setActiveTile } =
+    useConverter()
+  function buttonClicked(button: string) {
+    // let newValue = Number(`${value}` + button)
+    // if (button === SPECIAL_BUTTONS.CE.value) return setValue(0)
+    // setValue(newValue)
+  }
+  useEffect(() => {
+    handleUnit(base)
+  }, [])
+
+  return (
+    <Con>
+      <Con.Display>
+        {tiles.map((tile, index) => (
+          <Tile key={tile.id} tile={tile} />
+        ))}
+      </Con.Display>
+      <Con.Buttons>
+        {converterButtons.map((button, index) => {
+          if (button.value === "") return <span key={index}></span>
+          return (
+            <Button
+              onClick={() => buttonClicked(button.value)}
+              key={index}
+              isSpecial={!/[0-9.]/.test(button.value)}
+            >
+              {typeof button.label === "string" ? (
+                button.label
+              ) : (
+                <button.label size={20} />
+              )}
+            </Button>
+          )
+        })}
+      </Con.Buttons>
+    </Con>
+  )
 }
