531619679349c1784c894124f883f5cae105e4bb
Daryl Thomas <thomastw935@gmail.com>
2021-10-05 17:33:30 +0800

    Added Navigation component with style

--- src/components/Navigation/Navigation.style.ts
+++ src/components/Navigation/Navigation.style.ts
@@ -1,1 +1,47 @@
-import styled from "styled-components"
+import styled, { StyledComponentBase } from "styled-components"
+
+interface ICon extends StyledComponentBase<any, {}> {
+  SideNav?: any
+  ListTitle?: any
+  Title?: any
+}
+
+const Con: ICon = styled.nav`
+  display: flex;
+  gap: 1rem;
+  position: relative;
+`
+const sideNav = styled.section`
+  ul {
+    position: absolute;
+    top: 100%;
+    left: 0;
+    height: 100vh;
+    width: 50%;
+    background: ${(props) => props.theme.palette.primary.light};
+    padding: 1rem;
+    margin: 0;
+    display: flex;
+    flex-direction: column;
+    gap: 0.5rem;
+    list-style: none;
+    li {
+      text-transform: capitalize;
+    }
+  }
+`
+
+const listTitle = styled.li`
+  
+`
+
+const title = styled.span`
+  font-weight: bold;
+  font-size: 1.3rem;
+`
+
+Con.SideNav = sideNav
+Con.ListTitle = listTitle
+Con.Title = title
+
+export default Con
--- src/components/Navigation/index.tsx
+++ src/components/Navigation/index.tsx
@@ -1,3 +1,24 @@
+import Con from "./Navigation.style"
+import { Navigation as NavIcon } from "@styled-icons/fluentui-system-filled"
+import { UNITS } from "../../Data/Converter"
+import { CALCULATOR } from "../../Data/Calculator"
+
 export default function Navigation() {
-  return <div></div>
+  const handleListItems = (obj: {}) => {
+    return Object.keys(obj).map((key) => <li key={key}>{key.toLowerCase()}</li>)
+  }
+  return (
+    <Con>
+      <Con.SideNav>
+        <NavIcon size={30} />
+        <ul>
+          <li>Calculator</li>
+          {handleListItems(CALCULATOR)}
+          <li>Converter</li>
+          {handleListItems(UNITS)}
+        </ul>
+      </Con.SideNav>
+      <Con.Title>Length</Con.Title>
+    </Con>
+  )
 }
