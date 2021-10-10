f062097358f0359e7aa5d7b823daaeab9b8a5a2d
Daryl Thomas <thomastw935@gmail.com>
2021-10-10 23:46:04 +0800

    added a separate icons ref

---
+++ src/Data/index.ts
@@ -0,0 +1,9 @@
+import { Ruler, Calculator, Time } from "@styled-icons/remix-line"
+
+const BASE_ICONS: { [key: string]: any } = {
+  TIME: Time,
+  LENGTH: Ruler,
+  STANDARD: Calculator,
+}
+
+export { BASE_ICONS }
