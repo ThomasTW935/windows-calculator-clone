a15ebe82834f8e959bd3607aa4a1baf51912041f
Daryl Thomas <thomastw935@gmail.com>
2021-10-10 23:43:58 +0800

    Remove buttons component

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,5 @@
 import Calculator from "./Calculator"
 import Converter from "./Converter"
 import Navigation from "./Navigation"
-import Buttons from "./Buttons"
 
-export { Calculator, Converter, Navigation, Buttons }
+export { Calculator, Converter, Navigation }
