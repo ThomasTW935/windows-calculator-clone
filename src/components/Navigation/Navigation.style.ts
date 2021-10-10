8f15e712741843edd4bfdc92c034e0f1114d1d9f
Daryl Thomas <thomastw935@gmail.com>
2021-10-10 23:46:55 +0800

    fix navigation

--- src/components/Navigation/Navigation.style.ts
+++ src/components/Navigation/Navigation.style.ts
@@ -1,8 +1,10 @@
-import styled, { StyledComponentBase } from "styled-components"
+import styled, { StyledComponentBase, css } from "styled-components"
 
 interface ICon extends StyledComponentBase<any, {}> {
   SideNav?: any
+  List?: any
   ListTitle?: any
+  ListItem?: any
   Title?: any
 }
 
@@ -10,29 +12,66 @@ const Con: ICon = styled.nav`
   display: flex;
   gap: 1rem;
   position: relative;
-`
-const sideNav = styled.section`
-  ul {
-    position: absolute;
-    top: 100%;
-    left: 0;
-    height: 100vh;
-    width: 50%;
-    background: ${(props) => props.theme.palette.primary.light};
-    padding: 1rem;
-    margin: 0;
-    display: flex;
-    flex-direction: column;
-    gap: 0.5rem;
-    list-style: none;
-    li {
-      text-transform: capitalize;
-    }
+  svg {
+    cursor: pointer;
   }
 `
 
-const listTitle = styled.li`
-  
+interface SideNavProps {
+  active: boolean
+}
+
+const sideNav = styled.section<SideNavProps>`
+  transform: translateX(-100%);
+  opacity: 0;
+  visibility: hidden;
+  position: absolute;
+  top: 100%;
+  left: 0;
+  height: 100vh;
+  width: 50%;
+  transition: 0.25s ease-in-out;
+  background: ${(props) => props.theme.palette.primary.light};
+  ${(props) =>
+    props.active &&
+    css`
+      transform: translateX(0);
+      opacity: 1;
+      visibility: visible;
+    `};
+`
+
+const list = styled.ul`
+  padding: 0;
+  margin: 0;
+  display: flex;
+  flex-direction: column;
+  list-style: none;
+  > * {
+    padding: 0.5rem;
+  }
+`
+
+const listTitle = styled.span`
+  font-weight: bold;
+`
+
+interface ItemProps {
+  active: boolean
+}
+
+const listItem = styled.li<ItemProps>`
+  display: flex;
+  align-items: center;
+  gap: 0.3em;
+  text-transform: capitalize;
+  cursor: pointer;
+  border-left: 4px solid transparent;
+  border-color: ${(props) => (props.active ? "lightblue" : "transparent")};
+  &:hover,
+  &:active {
+    background: ${(props) => props.theme.palette.primary.lighter};
+  }
 `
 
 const title = styled.span`
@@ -41,7 +80,9 @@ const title = styled.span`
 `
 
 Con.SideNav = sideNav
+Con.List = list
 Con.ListTitle = listTitle
+Con.ListItem = listItem
 Con.Title = title
 
 export default Con
--- src/components/Navigation/index.tsx
+++ src/components/Navigation/index.tsx
@@ -1,22 +1,55 @@
+import { useState } from "react"
 import Con from "./Navigation.style"
 import { Navigation as NavIcon } from "@styled-icons/fluentui-system-filled"
-import { UNITS } from "../../Data/Converter"
 import { CALCULATOR } from "../../Data/Calculator"
+import { BASE_ICONS } from "../../Data"
+import { UNITS } from "../../Data/Converter"
 
-export default function Navigation() {
+interface Props {
+  activeTab: string
+  setActiveTab: (arg1: string) => void
+}
+
+export default function Navigation({ activeTab, setActiveTab }: Props) {
+  const [activeSideNav, setActiveSideNav] = useState(false)
+
+  function handleItemClicked(key: string) {
+    setActiveTab(key)
+    setActiveSideNav(false)
+  }
+
   const handleListItems = (obj: {}) => {
-    return Object.keys(obj).map((key) => <li key={key}>{key.toLowerCase()}</li>)
+    return Object.keys(obj).map((key, index) => {
+      const ICON = BASE_ICONS[key]
+      return (
+        <Con.ListItem
+          active={activeTab === key}
+          onClick={() => handleItemClicked(key)}
+          key={key}
+        >
+          <ICON size={20} />
+          <span>{key.toLowerCase()}</span>
+        </Con.ListItem>
+      )
+    })
   }
   return (
     <Con>
-      <Con.SideNav>
-        <NavIcon size={30} />
-        <ul>
-          <li>Calculator</li>
+      <NavIcon
+        onClick={() => {
+          setActiveSideNav(!activeSideNav)
+        }}
+        size={30}
+      />
+      <Con.SideNav active={activeSideNav}>
+        <Con.List>
+          <Con.ListTitle>Calculator</Con.ListTitle>
           {handleListItems(CALCULATOR)}
-          <li>Converter</li>
+        </Con.List>
+        <Con.List>
+          <Con.ListTitle>Converter</Con.ListTitle>
           {handleListItems(UNITS)}
-        </ul>
+        </Con.List>
       </Con.SideNav>
       <Con.Title>Length</Con.Title>
     </Con>
