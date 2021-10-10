aa642793e2908d25c4680cdb564c86ba997dc2a2
Daryl Thomas <thomastw935@gmail.com>
2021-10-10 23:47:12 +0800

    added activeTab state

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
+import { useState } from "react"
 import Con from "./App.style"
 import { Converter, Navigation } from "./components"
+import { UNITS } from "./Data/Converter"
 
 function App() {
+  const [activeTab, setActiveTab] = useState("LENGTH")
   return (
     <Con>
-      <Navigation />
-      <Converter />
+      <Navigation activeTab={activeTab} setActiveTab={setActiveTab} />
+
+      {Object.keys(UNITS).map((key) => {
+        if (key === activeTab) return <Converter key={key} base={activeTab} />
+      })}
     </Con>
   )
 }
