54f850a533376fc23810a0aa431dc553a7d06176
Daryl Thomas <thomastw935@gmail.com>
2021-10-10 23:44:23 +0800

    fix reference

--- src/components/Converter/Tile.tsx
+++ src/components/Converter/Tile.tsx
@@ -1,32 +1,52 @@
+import { ChangeEvent, Dispatch, useRef } from "react"
 import Input from "../../core-ui/Input"
-import useConverter, { ITile } from "./hooks/useConverter"
+import useConverter, { ACTIONS, ITile, Action } from "./hooks/useConverter"
+import { IUNIT } from "../../Data/Converter"
 
 interface Props {
   tile: ITile
+  activeTile: string
+  setActiveTile: (arg1: string) => void
+  dispatch: Dispatch<Action>
+  units: IUNIT
 }
 
-export default function Tile({ tile }: Props) {
-  const { dispatch, activeTile, setActiveTile, handleUnit } = useConverter()
-  const units = handleUnit("LENGTH")
-  console.log(units)
+export default function Tile({
+  tile,
+  activeTile,
+  setActiveTile,
+  dispatch,
+  units,
+}: Props) {
+  const selectRef = useRef<HTMLSelectElement>(null)
+  function handleSelect() {
+    if (selectRef.current) {
+      dispatch({
+        type: ACTIONS.UPDATE_UNIT,
+        payload: {
+          id: tile.id,
+          unit: selectRef.current.value,
+          activeTile: activeTile,
+        },
+      })
+    }
+  }
   return (
     <div>
       <Input
         onClick={() => {
-          setActiveTile(0)
+          setActiveTile(tile.id)
         }}
-        active={activeTile === 0}
-        value={0}
+        active={activeTile === tile.id}
+        value={tile.value}
         readOnly
       />
-      <select>
-        {Object.entries(units).map(([key, value], index) =>
-          // <option key={index} value={key}>
-          //   {value}
-          // </option>
-          // console.log(value[1])
-          console.log(key)
-        )}
+      <select ref={selectRef} value={tile.unit} onChange={handleSelect}>
+        {Object.entries(units).map(([key, value], index) => (
+          <option key={index} value={value}>
+            {value}
+          </option>
+        ))}
       </select>
     </div>
   )
