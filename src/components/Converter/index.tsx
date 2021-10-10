0410051a0194ac8aa53389e29922a6dcb621a7df
Daryl Thomas <thomastw935@gmail.com>
2021-10-10 23:47:42 +0800

    fix converter component

--- src/components/Converter/index.tsx
+++ src/components/Converter/index.tsx
@@ -1,31 +1,52 @@
 import { useState, useEffect } from "react"
 import Button from "../../core-ui/Button"
-import { converterButtons, SPECIAL_BUTTONS } from "../../Data/Buttons"
-
+import { converterButtons, SPECIAL_BUTTONS, IButton } from "../../Data/Buttons"
+import { buttonClicked } from "../../helper/buttonFunctions"
 import Con from "./Converter.style"
 import Input from "../../core-ui/Input"
 import { unit } from "mathjs"
-import useConverter from "./hooks/useConverter"
+import useConverter, { ACTIONS } from "./hooks/useConverter"
 import Tile from "./Tile"
+import { UNITS } from "../../Data/Converter"
 
-export default function Converter() {
-  const base = "length"
-  const { tiles, dispatch, handleUnit, activeTile, setActiveTile } =
-    useConverter()
-  function buttonClicked(button: string) {
-    // let newValue = Number(`${value}` + button)
-    // if (button === SPECIAL_BUTTONS.CE.value) return setValue(0)
-    // setValue(newValue)
+interface IProps {
+  base: string
+}
+
+export default function Converter({ base }: IProps) {
+  const { tiles, dispatch } = useConverter()
+  const [activeTile, setActiveTile] = useState(tiles[0].id)
+  const units = UNITS[base]
+  function handleButton(button: IButton) {
+    const tile = tiles.filter((tile) => tile.id === activeTile)[0]
+    let newValue = buttonClicked(button, tile.value)
+    dispatch({
+      type: ACTIONS.UPDATE_CURRENT_VALUE,
+      payload: {
+        id: activeTile,
+        value: Number(newValue),
+        activeTile: activeTile,
+      },
+    })
   }
   useEffect(() => {
-    handleUnit(base)
+    const unitKeys = Object.values(units)
+    const length = unitKeys.length
+    const selectedUnits = [unitKeys[length / 2], unitKeys[length / 2 + 1]]
+    dispatch({ type: ACTIONS.SET_UNITS, payload: { units: selectedUnits } })
   }, [])
-
   return (
     <Con>
       <Con.Display>
         {tiles.map((tile, index) => (
-          <Tile key={tile.id} tile={tile} />
+          <Tile
+            key={tile.id}
+            tile={tile}
+            activeTile={activeTile}
+            setActiveTile={setActiveTile}
+            dispatch={dispatch}
+            units={units}
+          />
         ))}
       </Con.Display>
       <Con.Buttons>
@@ -33,7 +54,7 @@ export default function Converter() {
           if (button.value === "") return <span key={index}></span>
           return (
             <Button
-              onClick={() => buttonClicked(button.value)}
+              onClick={() => handleButton(button)}
               key={index}
               isSpecial={!/[0-9.]/.test(button.value)}
             >
